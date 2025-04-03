fede5b05c561613f6057baa33436211776c24116
aaronv123 <aaronvincepenetrante@gmail.com>
2025-04-03 15:09:44 +0800

    Add files via upload

---
+++ excercise2/Excercise2/src/Electric.java
@@ -0,0 +1,9 @@
+public class Electric extends Engine {
+
+    Electric(boolean E_electric, int e_battery) {
+        this.electric = true;
+        this.battery = 10;
+    }
+
+
+}
---
+++ excercise2/Excercise2/src/InternalCombustion.java
@@ -0,0 +1,7 @@
+public class InternalCombustion extends Engine {
+    InternalCombustion(boolean Ic_gas, int e_cylinder) {
+        this.gas = true;
+        this.cylinder = 6;
+    }
+
+}
