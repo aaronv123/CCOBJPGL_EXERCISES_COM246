213f3678facdb7b4b28c75fc77a18fab571a3ed3
aaronv123 <aaronvincepenetrante@gmail.com>
2025-04-03 15:07:46 +0800

    Add files via upload

---
+++ excercise2/Excercise1/src/Engine.java
@@ -0,0 +1,24 @@
+public class Engine {
+
+    protected boolean gas;
+    protected boolean electric;
+    protected int cylinder;
+    protected int battery;
+
+    public boolean getGas() {
+        return this.gas;
+    }
+
+    public boolean getElectric() {
+        return this.electric;
+    }
+
+    public int getCylinder() {
+        return this.cylinder;
+    }
+
+    public int getBattery() {
+        return this.battery;
+    }
+
+}
