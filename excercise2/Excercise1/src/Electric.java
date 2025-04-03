3922910c3b8416d20d75fbe23522eb5b91be1b2e
aaronv123 <aaronvincepenetrante@gmail.com>
2025-04-03 15:09:17 +0800

    Delete excercise2/Excercise1 directory

--- excercise2/Excercise1/src/Electric.java
+++
@@ -1,9 +0,0 @@
-public class Electric extends Engine {
-
-    Electric(boolean E_electric, int e_battery) {
-        this.electric = true;
-        this.battery = 10;
-    }
-
-
-}
--- excercise2/Excercise1/src/InternalCombustion.java
+++
@@ -1,7 +0,0 @@
-public class InternalCombustion extends Engine {
-    InternalCombustion(boolean Ic_gas, int e_cylinder) {
-        this.gas = true;
-        this.cylinder = 6;
-    }
-
-}
